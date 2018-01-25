ea4537075b7e0448a6d3ded1e9a58ad0c70c7834
Jan Bucher <jan.bucher@itelligence.ch>
2018-01-25 14:16:57 +0100

    Adding shapes and color

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(40, 40, 40);
 
+	fill(200,0,0);
+  rect(30, 20, 55, 55);
+
+	fill(0,200,0);
+	ellipse(200, 46, 55, 55);
 }
